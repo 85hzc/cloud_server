2a3e5e3e38842b94deebfabe81da27d06c69e0f9
stillfarmer2011 <stillfarmer2011@gmail.com>
2016-04-08 11:32:07 +0800

    add post handler

--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,14 @@ router.get('/', function(req, res, next) {
 /* POST home page */
 
 router.post('/', function(req, res, next) {
+
+    
+    console.log("Enter router.post");
+
+    client_intf.client_packet_handler(req, res);
+
+    console.log("Exit router.post");
+
 });
 
 module.exports = router;

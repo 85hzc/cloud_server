194cb650a71e6a4ee61593ba392d961a3f0318c6
zilianglei <cast_dlg@sina.com>
2017-06-29 15:22:47 +0800

    fix bug

--- public/manager/js/historyall_statistics.js
+++ public/manager/js/historyall_statistics.js
@@ -77,8 +77,8 @@ if(isconnected) {
 function pie(piedata){
 
 		var value1 = piedata.values[7].count ? piedata.values[7].count : 0;
-			var value2 = piedata.values[8].count ? piedata.values[8].count : 0;
-	
+		var value2 = piedata.values[8].count ? piedata.values[8].count : 0;
+
 require(
 	[
 		'echarts',
@@ -89,6 +89,7 @@ require(
 		// 基于准备好的dom，初始化echarts图表
 	var myChart1 = ec.init(document.getElementsByClassName('ptwop')[0]);
 		var idx = 1;
+
  var option1 = {
 		
 			title: {
@@ -134,32 +135,23 @@ require(
 					{
 						value: value2,
 						name: 'P2P转发流的总字节数'
-					},
+					}
 
 				]
 
 			}]
 		};
 
+
 			option1.title.subtext = '直播流的总字节数 : '+piedata.values[6].count+'Byte';
 	
 
 
-		  //   option1.series.data = [{
-				// 		value: value1,
-				// 		name: '从直播平台获取流的总字节数'
-				// 	},
-				// 	{
-				// 		value: value2,
-				// 		name: 'P2P转发流的总字节数'
-				// 	}
-				// ];
-			myChart1.setOption(option1);
+
 
 		// 为echarts对象加载数据 
 		myChart1.setOption(option1);
 		
-
 	
 	}
 );
@@ -269,430 +261,7 @@ function allBytesCallback(data1) {
 } //allBytesCallback
 
 /*----------------------------------饼图---------------------------------*/
-// 使用
-require(
-	[
-		'echarts',
-		'echarts/chart/pie' // 使用柱状图就加载bar模块，按需加载
-	],
-	function(ec) {
-		// 基于准备好的dom，初始化echarts图表
-		var myChart = ec.init(document.getElementsByClassName('table')[1]);
-		var idx = 1;
-		option = {
-			timeline: {
-				data: [
-					'0', '1', '2',
-					'3', '4'
 
-				],
-				label: {
-					formatter: function(s) {
-						return s.slice(0, 7);
-					}
-				}
-			},
-			options: [{
-					title: {
-						text: '浏览器占比变化',
-						subtext: ''
-					},
-					tooltip: {
-						trigger: 'item',
-						formatter: "{a} <br/>{b} : {c} ({d}%)"
-					},
-					legend: {
-						data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
-					},
-					toolbox: {
-						show: true,
-						feature: {
-							mark: {
-								show: true
-							},
-							dataView: {
-								show: true,
-								readOnly: false
-							},
-							magicType: {
-								show: true,
-								type: ['pie', 'funnel'],
-								option: {
-									funnel: {
-										x: '25%',
-										width: '50%',
-										funnelAlign: 'left',
-										max: 1700
-									}
-								}
-							},
-							restore: {
-								show: true
-							},
-							saveAsImage: {
-								show: true
-							}
-						}
-					},
-					series: [{
-						name: '浏览器（数据纯属虚构）',
-						type: 'pie',
-						center: ['50%', '45%'],
-						radius: '50%',
-						data: [{
-								value: idx * 128 + 80,
-								name: '周一'
-							},
-							{
-								value: idx * 64 + 160,
-								name: '周二'
-							},
-							{
-								value: idx * 32 + 320,
-								name: '周三'
-							},
-							{
-								value: idx * 16 + 640,
-								name: 'IE9+'
-							},
-							{
-								value: idx++ * 8 + 1280,
-								name: 'IE8-'
-							}
-						]
-					}]
-				},
-				{
-					series: [{
-						name: '浏览器（数据纯属虚构）',
-						type: 'pie',
-						data: [{
-								value: idx * 128 + 80,
-								name: 'Chrome'
-							},
-							{
-								value: idx * 64 + 160,
-								name: 'Firefox'
-							},
-							{
-								value: idx * 32 + 320,
-								name: 'Safari'
-							},
-							{
-								value: idx * 16 + 640,
-								name: 'IE9+'
-							},
-							{
-								value: idx++ * 8 + 1280,
-								name: 'IE8-'
-							}
-						]
-					}]
-				},
-				{
-					series: [{
-						name: '浏览器（数据纯属虚构）',
-						type: 'pie',
-						data: [{
-								value: idx * 128 + 80,
-								name: 'Chrome'
-							},
-							{
-								value: idx * 64 + 160,
-								name: 'Firefox'
-							},
-							{
-								value: idx * 32 + 320,
-								name: 'Safari'
-							},
-							{
-								value: idx * 16 + 640,
-								name: 'IE9+'
-							},
-							{
-								value: idx++ * 8 + 1280,
-								name: 'IE8-'
-							}
-						]
-					}]
-				},
-				{
-					series: [{
-						name: '浏览器（数据纯属虚构）',
-						type: 'pie',
-						data: [{
-								value: idx * 128 + 80,
-								name: 'Chrome'
-							},
-							{
-								value: idx * 64 + 160,
-								name: 'Firefox'
-							},
-							{
-								value: idx * 32 + 320,
-								name: 'Safari'
-							},
-							{
-								value: idx * 16 + 640,
-								name: 'IE9+'
-							},
-							{
-								value: idx++ * 8 + 1280,
-								name: 'IE8-'
-							}
-						]
-					}]
-				},
-				{
-					series: [{
-						name: '浏览器（数据纯属虚构）',
-						type: 'pie',
-						data: [{
-								value: idx * 128 + 80,
-								name: 'Chrome'
-							},
-							{
-								value: idx * 64 + 160,
-								name: 'Firefox'
-							},
-							{
-								value: idx * 32 + 320,
-								name: 'Safari'
-							},
-							{
-								value: idx * 16 + 640,
-								name: 'IE9+'
-							},
-							{
-								value: idx++ * 8 + 1280,
-								name: 'IE8-'
-							}
-						]
-					}]
-				},
-				{
-					series: [{
-						name: '浏览器（数据纯属虚构）',
-						type: 'pie',
-						data: [{
-								value: idx * 128 + 80,
-								name: 'Chrome'
-							},
-							{
-								value: idx * 64 + 160,
-								name: 'Firefox'
-							},
-							{
-								value: idx * 32 + 320,
-								name: 'Safari'
-							},
-							{
-								value: idx * 16 + 640,
-								name: 'IE9+'
-							},
-							{
-								value: idx++ * 8 + 1280,
-								name: 'IE8-'
-							}
-						]
-					}]
-				},
-				{
-					series: [{
-						name: '浏览器（数据纯属虚构）',
-						type: 'pie',
-						data: [{
-								value: idx * 128 + 80,
-								name: 'Chrome'
-							},
-							{
-								value: idx * 64 + 160,
-								name: 'Firefox'
-							},
-							{
-								value: idx * 32 + 320,
-								name: 'Safari'
-							},
-							{
-								value: idx * 16 + 640,
-								name: 'IE9+'
-							},
-							{
-								value: idx++ * 8 + 1280,
-								name: 'IE8-'
-							}
-						]
-					}]
-				},
-				{
-					series: [{
-						name: '浏览器（数据纯属虚构）',
-						type: 'pie',
-						data: [{
-								value: idx * 128 + 80,
-								name: 'Chrome'
-							},
-							{
-								value: idx * 64 + 160,
-								name: 'Firefox'
-							},
-							{
-								value: idx * 32 + 320,
-								name: 'Safari'
-							},
-							{
-								value: idx * 16 + 640,
-								name: 'IE9+'
-							},
-							{
-								value: idx++ * 8 + 1280,
-								name: 'IE8-'
-							}
-						]
-					}]
-				},
-				{
-					series: [{
-						name: '浏览器（数据纯属虚构）',
-						type: 'pie',
-						data: [{
-								value: idx * 128 + 80,
-								name: 'Chrome'
-							},
-							{
-								value: idx * 64 + 160,
-								name: 'Firefox'
-							},
-							{
-								value: idx * 32 + 320,
-								name: 'Safari'
-							},
-							{
-								value: idx * 16 + 640,
-								name: 'IE9+'
-							},
-							{
-								value: idx++ * 8 + 1280,
-								name: 'IE8-'
-							}
-						]
-					}]
-				},
-				{
-					series: [{
-						name: '浏览器（数据纯属虚构）',
-						type: 'pie',
-						data: [{
-								value: idx * 128 + 80,
-								name: 'Chrome'
-							},
-							{
-								value: idx * 64 + 160,
-								name: 'Firefox'
-							},
-							{
-								value: idx * 32 + 320,
-								name: 'Safari'
-							},
-							{
-								value: idx * 16 + 640,
-								name: 'IE9+'
-							},
-							{
-								value: idx++ * 8 + 1280,
-								name: 'IE8-'
-							}
-						]
-					}]
-				},
-				{
-					series: [{
-						name: '浏览器（数据纯属虚构）',
-						type: 'pie',
-						data: [{
-								value: idx * 128 + 80,
-								name: 'Chrome'
-							},
-							{
-								value: idx * 64 + 160,
-								name: 'Firefox'
-							},
-							{
-								value: idx * 32 + 320,
-								name: 'Safari'
-							},
-							{
-								value: idx * 16 + 640,
-								name: 'IE9+'
-							},
-							{
-								value: idx++ * 8 + 1280,
-								name: 'IE8-'
-							}
-						]
-					}]
-				},
-				{
-					series: [{
-						name: '浏览器（数据纯属虚构）',
-						type: 'pie',
-						data: [{
-								value: idx * 128 + 80,
-								name: 'Chrome'
-							},
-							{
-								value: idx * 64 + 160,
-								name: 'Firefox'
-							},
-							{
-								value: idx * 32 + 320,
-								name: 'Safari'
-							},
-							{
-								value: idx * 16 + 640,
-								name: 'IE9+'
-							},
-							{
-								value: idx++ * 8 + 1280,
-								name: 'IE8-'
-							}
-						]
-					}]
-				},
-				{
-					series: [{
-						name: '浏览器（数据纯属虚构）',
-						type: 'pie',
-						data: [{
-								value: idx * 128 + 80,
-								name: 'Chrome1'
-							},
-							{
-								value: idx * 64 + 160,
-								name: 'Firefox1'
-							},
-							{
-								value: idx * 32 + 320,
-								name: 'Safari1'
-							},
-							{
-								value: idx * 16 + 640,
-								name: 'IE9+1'
-							},
-							{
-								value: idx++ * 8 + 1280,
-								name: 'IE8-1'
-							}
-						]
-					}]
-				} //series结束
-			]
-		};
-
-		// 为echarts对象加载数据 
-		myChart.setOption(option);
-	}
-);
 
 /*-----------------------------饼图end------------------------*/
 
@@ -716,7 +285,7 @@ function allsumCallback(data1) {
 		],
 		function(ec) {
 			// 基于准备好的dom，初始化echarts图表
-			var myChart = ec.init(document.getElementsByClassName('table')[2]);
+			var myChart = ec.init(document.getElementsByClassName('table')[1]);
 
 			var option = {
 				grid: {

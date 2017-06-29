0a84f05f6eabfa0622fe605d8b00604f4004f257
zhang <zhang@zhangMacBook-Pro.local>
2017-06-29 10:53:33 +0800

    前端

--- public/manager/js/historyall_statistics.js
+++ public/manager/js/historyall_statistics.js
@@ -1,31 +1,31 @@
 //处理数据
-function formateData(data1,bytes) {
+function formateData(data1, bytes) {
 	var arr = data1.values;
 	var timeArr = [];
 	var bytesArr = [];
 
 	for(var i = 0; i < arr.length; i++) {
-		if(bytes == 1){
+		if(bytes == 1) {
 			bytesArr.push(arr[i].totalBytes);
 			timeArr.push(arr[i].time.slice(0, 7));
 		}
-		if(bytes == 2){
+		if(bytes == 2) {
 			bytesArr.push(arr[i].num);
 			timeArr.push(arr[i].week + "周");
 		}
-		
+
 	}
-	
+
 	return {
 		timeArr: timeArr,
 		bytesArr: bytesArr
 	};
 }
 /*----------------------------------------------------------------*/
-var isconnected = true;//是否联网状态
+var isconnected = false; //是否联网状态
 if(isconnected) {
 	myajax(8, "get", {}, allBytesCallback);
-	myajax(9,"get",{},allsumCallback);
+	myajax(9, "get", {}, allsumCallback);
 } else {
 
 	var data = {
@@ -33,44 +33,138 @@ if(isconnected) {
 			totalBytes: 100100000,
 			time: "2016-05-21 20:31:00"
 		}, {
-			totalBytes:2000000000,
+			totalBytes: 2000000000,
 			time: "2016-06-21 20:31:00"
 		}, {
-			totalBytes:1000000000,
+			totalBytes: 1000000000,
 			time: "2016-07-21 20:31:00"
 		}, {
-			totalBytes:1000000000,
+			totalBytes: 1000000000,
 			time: "2016-08-21 20:31:00"
 		}]
 	};
-	
+
 	var data2 = {
-		values:[{
-			num:20,
-			week:21
-		},{
-			num:3,
-			week:25
-		},{
-			num:4,
-			week:23
-		},{
-			num:5,
-			week:24
+		values: [{
+			num: 20,
+			week: 21
+		}, {
+			num: 3,
+			week: 25
+		}, {
+			num: 4,
+			week: 23
+		}, {
+			num: 5,
+			week: 24
 		}]
 	};
-	
-	
+
 	allBytesCallback(data);
 	allsumCallback(data2);
+	
 }
 
+/*----------------------------------流的情况饼图---------------------------------*/
 
+
+
+var option1;
+var myChart1;
+function refreshPie(data){
+			option1.title.subtext
+		    option1.series.data = [{
+						value: data.values.从直播平台获取流的总字节数,
+						name: '从直播平台获取流的总字节数'
+					},
+					{
+						value: data.values.P2P转发流的总字节数,
+						name: 'P2P转发流的总字节数'
+					},
+				];
+			myChart1.setOption(option);
+}
+       
+
+		
+
+	
+	
+require(
+	[
+		'echarts',
+		'echarts/chart/pie' // 使用柱状图就加载bar模块，按需加载
+	],
+	function(ec) {
+			
+		// 基于准备好的dom，初始化echarts图表
+		var myChart1 = ec.init(document.getElementsByClassName('ptwop')[0]);
+		var idx = 1;
+ var  option1 = {
+		
+			title: {
+				text: '直播流情况',
+				subtext: '直播流的总字节数 : 2000000000000Byte',
+				x: 'center',
+				subtextStyle: {
+            		color: 'orange'          // 副标题文字颜色
+        }
+			},
+			tooltip: {
+				trigger: 'item',
+				formatter: "{a} <br/>{b} : {c} ({d}%)"
+			},
+			legend: {
+				orient: 'vertical',
+				x: 'left',
+				data: ['从直播平台获取流的总字节数', 'P2P转发流的总字节数']
+			},
+
+			calculable: true,
+			series: [{
+			
+				itemStyle: {
+					normal: {
+						label: {
+							show: true,
+							formatter: '{c} Byte ({d}%)'
+						},
+						labelLine: {
+							show: true
+						}
+					}
+				},
+				name: '直播流情况:',
+				type: 'pie',
+				radius: '55%',
+				center: ['50%', '60%'],
+				data: [{
+						value: 335,
+						name: '从直播平台获取流的总字节数'
+					},
+					{
+						value: 310,
+						name: 'P2P转发流的总字节数'
+					},
+
+				]
+
+			}]
+		};
+
+		// 为echarts对象加载数据 
+		myChart1.setOption(option1);
+		
+
+	
+	}
+);
+
+/*----------------------------------流的情况结束---------------------------------*/
 
 function allBytesCallback(data1) {
-	
-var data1 = formateData(data1,1);//处理数据
 
+	var data1 = formateData(data1, 1); //处理数据
 
 	require.config({
 		paths: {
@@ -90,8 +184,8 @@ var data1 = formateData(data1,1);//处理数据
 			var myChart = ec.init(document.getElementsByClassName('table')[0]);
 
 			var option = {
-				grid:{
-					x:150
+				grid: {
+					x: 150
 				},
 				title: {
 					text: '每月转发流量',
@@ -141,23 +235,23 @@ var data1 = formateData(data1,1);//处理数据
 					name: '转发字节',
 					type: 'line',
 					data: data1.bytesArr,
-//					markPoint: {
-//						data: [{
-//								type: 'max',
-//								name: '最大值'
-//							},
-//							{
-//								type: 'min',
-//								name: '最小值'
-//							}
-//						]
-//					},
-//					markLine: {
-//						data: [{
-//							type: 'average',
-//							name: '平均值'
-//						}]
-//					}
+					//					markPoint: {
+					//						data: [{
+					//								type: 'max',
+					//								name: '最大值'
+					//							},
+					//							{
+					//								type: 'min',
+					//								name: '最小值'
+					//							}
+					//						]
+					//					},
+					//					markLine: {
+					//						data: [{
+					//							type: 'average',
+					//							name: '平均值'
+					//						}]
+					//					}
 				}]
 			};
 
@@ -596,13 +690,10 @@ require(
 
 /*-----------------------------饼图end------------------------*/
 
-
-
 /*-----------------------------每周观众人数------------------------*/
 function allsumCallback(data1) {
-	
-var data1 = formateData(data1,2);//处理数据
 
+	var data1 = formateData(data1, 2); //处理数据
 
 	require.config({
 		paths: {
@@ -622,8 +713,8 @@ var data1 = formateData(data1,2);//处理数据
 			var myChart = ec.init(document.getElementsByClassName('table')[2]);
 
 			var option = {
-				grid:{
-					x:150
+				grid: {
+					x: 150
 				},
 				title: {
 					text: '每周观众人数',
@@ -673,23 +764,23 @@ var data1 = formateData(data1,2);//处理数据
 					name: '观众人数',
 					type: 'line',
 					data: data1.bytesArr,
-//					markPoint: {
-//						data: [{
-//								type: 'max',
-//								name: '最大值'
-//							},
-//							{
-//								type: 'min',
-//								name: '最小值'
-//							}
-//						]
-//					},
-//					markLine: {
-//						data: [{
-//							type: 'average',
-//							name: '平均值'
-//						}]
-//					}
+					//					markPoint: {
+					//						data: [{
+					//								type: 'max',
+					//								name: '最大值'
+					//							},
+					//							{
+					//								type: 'min',
+					//								name: '最小值'
+					//							}
+					//						]
+					//					},
+					//					markLine: {
+					//						data: [{
+					//							type: 'average',
+					//							name: '平均值'
+					//						}]
+					//					}
 				}]
 			};
 
